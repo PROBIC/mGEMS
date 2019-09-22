1594eb5f8c2c6d127dbaaa33d31fd872263551ea
Tommi Mäklin <tommi@maklin.fi>
2019-09-22 18:58:48 +0300

    Add func to determine presence of a toggle.

--- src/build_sample/main.cpp
+++ src/build_sample/main.cpp
@@ -8,6 +8,10 @@ char* GetOpt(char **begin, char **end, const std::string &option) {
   return ((it != end && ++it != end) ? *it : 0);
 }
 
+bool CmdOptionPresent(char **begin, char **end, const std::string &option) {
+  return (std::find(begin, end, option) != end);
+}
+
 int main(int argc, char* argv[]) {
   std::string assignment_file = std::string(GetOpt(argv, argv+argc, "-a"));
   std::string outfile = std::string(GetOpt(argv, argv+argc, "-o"));
