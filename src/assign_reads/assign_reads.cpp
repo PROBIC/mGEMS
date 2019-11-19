52be2e0725730cdb7a1272a96d3ba6cc7fcd6b00
Tommi Mäklin <tommi@maklin.fi>
2019-11-19 13:15:30 +0200

    use cxxargs to parse arguments

--- src/assign_reads/assign_reads.cpp
+++ src/assign_reads/assign_reads.cpp
@@ -3,50 +3,73 @@
 #include <unordered_map>
 #include <iostream>
 #include <memory>
+#include <exception>
 
-#include "arguments.h"
+#include "zstr/zstr.hpp"
+#include "cxxargs/include/cxxargs.hpp"
+
 #include "assign_reads/read_files.h"
 #include "assign_reads/write_files.h"
 #include "assign_reads/construct_thresholds.h"
-#include "zstr/zstr.hpp"
+
+void parse_args(int argc, char* argv[], cxxargs::Arguments &args) {
+  args.add_short_argument<std::string>('f', "Read-EC assignments file path.");
+  args.add_short_argument<std::string>('o', "Output directory.");
+  args.add_short_argument<std::string>('a', "mSWEEP relative abundance estimates path.");
+  args.add_short_argument<long double>('q', "Multiplier term in assignment thresholds. (default: 1.0)", (long double)1.0);
+  args.add_long_argument<std::string>("groups", "Reference groups to assign reads to.");
+  args.add_short_argument<std::string>('p', "Read-EC probability matrix (not required if --cin is specified)");
+  args.set_not_required('p');
+  args.add_long_argument<bool>("cin", "Read the probs file from cin. (default: false)", false);
+  args.add_long_argument("gzip-output", "Compress the output files (default: false)", false);
+
+  args.parse(argc, argv);
+  if (!args.value<bool>("cin") && !args.is_initialized('p')) {
+    throw std::runtime_error("The Read-EC probability matrix must be specified if not read from cin");
+  }
+}
 
 int main(int argc, char* argv[]) {
+  cxxargs::Arguments args("assign-reads", "Usage:...");
+  try {
+    std::cout << "Parsing arguments" << std::endl;
+    parse_args(argc, argv, args);
+  } catch (std::exception &e) {
+    std::cout << "Parsing arguments failed:\n"
+	      << '\t' << e.what()
+	      << "\nexiting\n";
+    return 0;    
+  }
+
   std::unordered_map<long unsigned, std::pair<std::vector<std::string>, std::vector<bool>>> reads_in_ec;
   std::cout << "Reading read assignments to equivalence classes" << std::endl;
-  if (CmdOptionPresent(argv, argv+argc, "-f")) {
-    std::unique_ptr<std::istream> assignments_file = OpenInstream(argv, argv+argc, "-f");
-    read_ec_assignments(*assignments_file, &reads_in_ec);
-  }
+  std::unique_ptr<std::istream> assignments_file(new zstr::ifstream(args.value<std::string>('f')));
+  read_ec_assignments(*assignments_file, &reads_in_ec);
 
   std::cout << "Constructing assignment thresholds from abundances" << std::endl;
   std::vector<std::pair<std::string, long double>> thresholds;
-  if (CmdOptionPresent(argv, argv+argc, "-a")) {
-    std::unique_ptr<std::istream> abundances_file = OpenInstream(argv, argv+argc, "-a");
-    long double mult = (CmdOptionPresent(argv, argv+argc, "-q") ? std::stold(std::string(GetOpt(argv, argv+argc, "-q"))) : (long double)1.0);
-    construct_thresholds(reads_in_ec.size(), *abundances_file, &thresholds, mult);
-  }
+  std::unique_ptr<std::istream> abundances_file(new zstr::ifstream(args.value<std::string>('a')));
+  construct_thresholds(reads_in_ec.size(), *abundances_file, &thresholds, args.value<long double>('q'));
   uint16_t n_refs = thresholds.size();
 
   std::cout << "Reading probs" << std::endl;
-  bool read_from_cin = CmdOptionPresent(argv, argv+argc, "--cin");
-  if (read_from_cin) {
+  if (args.value<bool>("cin")) {
     assign_reads(thresholds, std::cin, &reads_in_ec);
   } else {
-    std::unique_ptr<std::istream> probs_file = OpenInstream(argv, argv+argc, "-p");
+    std::unique_ptr<std::istream> probs_file(new zstr::ifstream(args.value<std::string>('p')));
     assign_reads(thresholds, *probs_file, &reads_in_ec);
   }
 
   std::cout << "Assigning reads to reference groups" << std::endl;
   std::vector<short unsigned> group_indices;
-  std::unique_ptr<std::istream> groups_file = OpenInstream(argv, argv+argc, "--groups");
+  std::unique_ptr<std::istream> groups_file(new zstr::ifstream(args.value<std::string>("groups")));
   read_groups_filter(thresholds, *groups_file, &group_indices);
   
-  bool gzip_output = CmdOptionPresent(argv, argv+argc, "--gzip-output");
-  std::string outfile_name = std::string(GetOpt(argv, argv+argc, "-o"));
+  std::string outfile_name = args.value<std::string>('o');
   std::vector<std::unique_ptr<std::ostream>> outfiles(n_refs);
   for (auto i : group_indices) {
     std::string fname = outfile_name + "/" + thresholds.at(i).first + "_reads.txt";
-    if (gzip_output) {
+    if (args.value<bool>("gzip")) {
       outfiles[i] = std::unique_ptr<std::ostream>(new zstr::ofstream(fname + ".gz"));
     } else {
       outfiles[i] = std::unique_ptr<std::ostream>(new std::ofstream(fname));
--- src/build_sample/build_sample.cpp
+++ src/build_sample/build_sample.cpp
@@ -1,25 +1,44 @@
 #include <string>
 #include <algorithm>
+#include <exception>
+#include <iostream>
+
+#include "cxxargs/include/cxxargs.hpp"
+#include "zstr/zstr.hpp"
 
 #include "arguments.h"
 #include "build_sample/assign_reads.h"
-#include "zstr/zstr.hpp"
+
+void parse_args(int argc, char* argv[], cxxargs::Arguments &args) {
+  args.add_short_argument<std::string>('a', "Read assignments to equivalence classes.");
+  args.add_short_argument<std::string>('o', "Output directory.");
+  args.add_short_argument<std::string>('1', "Strand 1 to demix reads from.");
+  args.add_short_argument<std::string>('2', "Strand 2 to demix reads from.");
+  args.add_long_argument<bool>("gzip-output", "Compress output files (default: false)");
+  args.parse(argc, argv);
+}
 
 int main(int argc, char* argv[]) {
-  std::string assignment_path = std::string(GetOpt(argv, argv+argc, "-a"));
-  std::string outfile = std::string(GetOpt(argv, argv+argc, "-o"));
-  std::string strand1 = std::string(GetOpt(argv, argv+argc, "-1"));
-  std::string strand2 = std::string(GetOpt(argv, argv+argc, "-2"));
-  bool gzip_output = CmdOptionPresent(argv, argv+argc, "--gzip-output");
+  cxxargs::Arguments args("build-sample", "Usage:...");
+  try {
+    std::cout << "Parsing arguments" << std::endl;
+    parse_args(argc, argv, args);
+  } catch (std::exception &e) {
+    std::cout << "Parsing arguments failed:\n"
+	      << '\t' << e.what()
+	      << "\nexiting\n";
+    return 0;
+  }
 
-  zstr::ifstream assignment_file(assignment_path);
+  zstr::ifstream assignment_file(args.value<std::string>('a'));
   const std::set<std::string> &assignments = read_assignments(assignment_file);
 
   std::unique_ptr<std::istream> infiles[2];
-  infiles[0] = std::unique_ptr<std::istream>(new zstr::ifstream(strand1));
-  infiles[1] = std::unique_ptr<std::istream>(new zstr::ifstream(strand2));
+  infiles[0] = std::unique_ptr<std::istream>(new zstr::ifstream(args.value<std::string>('1')));
+  infiles[1] = std::unique_ptr<std::istream>(new zstr::ifstream(args.value<std::string>('2')));
   std::unique_ptr<std::ostream> outfiles[2];
-  if (gzip_output) {
+  std::string outfile = args.value<std::string>('o');
+  if (args.value<bool>("gzip-output")) {
     outfiles[0] = std::unique_ptr<std::ostream>(new zstr::ofstream(outfile + "_1.fastq.gz"));
     outfiles[1] = std::unique_ptr<std::ostream>(new zstr::ofstream(outfile + "_2.fastq.gz"));
   } else {
